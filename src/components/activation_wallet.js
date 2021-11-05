c80363de45155b28e9a440b7c416579ff120685a
Bhuvaneswari13 <bhuvantnj13@gmail.com>
2021-11-05 20:05:09 +0530

    changes-5/11

--- src/components/activation_wallet.js
+++ src/components/activation_wallet.js
@@ -9,24 +9,31 @@ const ActivationWallet = () =>{
         
         <UpBar/>
 
-        <div className="container3">
-            <div className="first_contain3">
-            <div className="sub_first_contain3">
-                <div className="title_contain3">
+        <div className="container2">
+            <div className="first_contain2">
+            <div className="sub_first_contain2">
+                <div className="title_contain2">
                     <h3>Activation Wallet Report of 707123 (Balance - TRX 0.00) </h3>
                     <br/>
+                    
                     <table id="customers" >
-  <tr>
-    <th>Serial</th>
-    <th>TxnID</th>
-    <th>Date/Time</th>
-    <th>Amount</th>
-    <th>Address</th>
-    <th>Status</th>
+ <tr>
+    <th>Serial <i class="fas fa-caret-down"></i> <i class="fas fa-caret-up"></i></th>
+    <th>TxnID  <i class="fas fa-caret-down"></i> <i class="fas fa-caret-up"></i></th>
+    <th>Date/Time  <i class="fas fa-caret-down"></i> <i class="fas fa-caret-up"></i></th>
+    <th>Amount  <i class="fas fa-caret-down"></i> <i class="fas fa-caret-up"></i></th>
+    <th>Address  <i class="fas fa-caret-down"></i> <i class="fas fa-caret-up"></i></th>
+    <th>Status  <i class="fas fa-caret-down"></i> <i class="fas fa-caret-up"></i></th>
   </tr>
   <tr>
-    <td>No Records Found</td>
+    <td>1</td>
+    <td>19/09/2021</td>
+    <td>TRX 200.00</td>
+    <td>Invest</td>
+    <td>707123</td>
+    <td>Debit</td>
     </tr>
+    
   </table>
                     
                     <p className="title_contain2">Note:Do not Send Coins after 3 hours of creating the QR Code.
