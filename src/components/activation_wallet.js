705b9b878de26453b04075cad9d4398908adf8b7
Bhuvaneswari13 <bhuvantnj13@gmail.com>
2021-11-07 20:37:25 +0530

    Changes-7/11

--- src/components/activation_wallet.js
+++ src/components/activation_wallet.js
@@ -2,6 +2,7 @@
 import React from "react";
 import './pendingdepo.css'
 import UpBar from "./UpBar";
+import './activation_wallet.css'
 
 const ActivationWallet = () =>{
     return(
@@ -9,14 +10,25 @@ const ActivationWallet = () =>{
         
         <UpBar/>
 
-        <div className="container2">
-            <div className="first_contain2">
-            <div className="sub_first_contain2">
-                <div className="title_contain2">
+        <div className="container6">
+            <div className="first_contain6">
+            <div className="sub_first_contain6">
+                <div className="title_contain6">
                     <h3>Activation Wallet Report of 707123 (Balance - TRX 0.00) </h3>
                     <br/>
                     
-                    <table id="customers" >
+                    <span className="left"> 
+                        <label id="up-display" className="title_contain6">Display  <select name="nos" id="nos">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                       
+                      </select> Records Per Page</label>
+                        </span>
+                    <span className="right">
+                    <label id="up-display" className="title_contain6_sub">Search: <input type="text" id="search-top"/></label>
+                    </span>
+                    <br/>
+                    <table id="customers3">
  <tr>
     <th>Serial <i class="fas fa-caret-down"></i> <i class="fas fa-caret-up"></i></th>
     <th>TxnID  <i class="fas fa-caret-down"></i> <i class="fas fa-caret-up"></i></th>
@@ -33,11 +45,24 @@ const ActivationWallet = () =>{
     <td>707123</td>
     <td>Debit</td>
     </tr>
+    <tr>
+    <td>2</td>
+    <td>19/09/2021</td>
+    <td>TRX 200.00</td>
+    <td>Transfer In</td>
+    <td>277538</td>
+    <td>Credit</td>
+    </tr>
     
   </table>
-                    
-                    <p className="title_contain2">Note:Do not Send Coins after 3 hours of creating the QR Code.
-                    </p>
+        
+                    <span className="left"> 
+                        <p className="title_contain6">Showing Page 1 of 1</p>
+                        </span>
+                    <span className="right">
+                    <p id="down-show" className="title_contain6">Previous <button id="badge1">1</button> Next</p>
+                    </span>
+    
                 </div>
             </div>
             </div>
